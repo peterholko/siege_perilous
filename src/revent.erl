8eb9d357989481c555934ab0dafbd57715249977
Peter Holko <peter.holko@gmail.com>
2016-03-19 14:54:21 -0600

    Adding revent module

---
+++ src/revent.erl
@@ -0,0 +1,78 @@
+%% Author: Peter
+%% Created: March, 2016
+%% Description: Random events interface
+-module(revent).
+
+-include("schema.hrl").
+
+-export([create/0, get/1, to_map/1]).
+-export([apply_effect/3]).
+
+create() ->
+    AllREvents = ets:tab2list(revent),
+    Num = length(AllREvents),
+    Rand = util:rand(Num),
+    REvent = lists:nth(Rand, AllREvents),
+    REvent.
+
+get(ObjId) ->
+    [State] = db:read(state, ObjId),
+    REventId = State#state.data,
+    [REvent] = db:read(revent, REventId),
+    REvent.
+
+to_map(REvent) ->
+    REvent0 = maps:put(<<"text">>, REvent#revent.text, #{}),
+    REvent1 = maps:put(<<"responses">>, REvent#revent.responses_text, REvent0),
+    REvent2 = maps:put(<<"effects">>, REvent#revent.effects_text, REvent1),
+    REvent2.
+
+apply_effect(ObjId, REvent, ResponseNum) ->
+    Effect = lists:nth(ResponseNum, REvent#revent.effects),
+    lager:info("Apply affect ~p", [Effect]),
+
+    case Effect of
+        {attrmod, AttrMod} ->
+            lager:info("Apply attrmod: ~p", [AttrMod]);
+        {random, RandomList} ->
+            Rand = util:rand(100),
+            SelectedEffect = get_random(Rand, 0, RandomList, none),
+            lager:info("SelectedEffect: ~p", [SelectedEffect]),
+            apply_random(ObjId, SelectedEffect);
+        _ -> 
+            lager:info("Effect nothing")
+    end.
+            
+apply_random(_ObjId, none) -> nothing;
+apply_random(ObjId, SelectedEffect) ->
+    EffectType = element(2, SelectedEffect),
+
+    case EffectType of
+        spawn ->
+            lager:info("Spawning npc..."),
+            SpawnType = element(3, SelectedEffect),
+            [Obj] = db:read(obj, ObjId),            
+            encounter:spawn_npc(SpawnType, Obj#obj.pos);
+        loot ->
+            lager:info("Generating loot"),
+            encounter:generate_loot(ObjId);
+            
+        _ ->
+            lager:info("Unknown effect type")
+    end.
+    
+get_random(_RandNum, _SumChance, [], SelectedRandom) ->
+    SelectedRandom;
+get_random(RandNum, SumChance, [Random | Rest], none) ->
+    Chance = element(1, Random),
+    NewSumChance = Chance + SumChance,    
+    lager:info("RandNum ~p Chance: ~p", [RandNum, NewSumChance]),
+
+    NewSelectedRandom = case RandNum =< NewSumChance of
+                            true -> Random;
+                            false -> none
+                        end,
+
+    get_random(RandNum, NewSumChance, Rest, NewSelectedRandom);
+get_random(_RandNum, _SumChance, _RandomList, SelectedRandom) ->
+    SelectedRandom.
